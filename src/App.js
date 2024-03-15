94d491ea2cd30194c3941240ecff88c812d6567f
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-03-15 11:28:44 +0530

    Tour.js added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
+const url = 'https://course-api.com/react-tours-project';
+
 function App() {
+
+  useState(true);
+  useState([]);
+  
   return (
     <main>
       Hi
